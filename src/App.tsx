b1c74fd38cced3c6ffa27b6f6e3079048cb890bd
hyunjun <khjae0207@naver.com>
2024-02-13 16:16:48 +0900

    BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-
-const Container=styled.div`
-  background-color:${props=>props.theme.bgColor};
-`;
-
-const H1=styled.h1`
-  color:${props=>props.theme.textColor};
-`;
+import React from "react";
+import Router from "./Router";
 
 function App() {
-  
-  return <Container>
-    <H1>protected</H1>
-  </Container>
+  return (
+    <div>
+      <Router />
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme } from './theme';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import React from "react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
-
