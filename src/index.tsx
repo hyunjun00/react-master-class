b392566d3b5d162bed7816056317656dafbc754e
hyunjun <khjae0207@naver.com>
2024-02-14 13:32:34 +0900

    createBrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
