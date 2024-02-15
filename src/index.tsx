c91a964b2fb09cfae530cdca218c15b217a25f76
hyunjun <khjae0207@naver.com>
2024-02-15 18:38:39 +0900

    Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import ReactDOM from "react-dom/client";
 import React from "react";
-import { RouterProvider } from "react-router-dom";
-import router from "./Router";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
