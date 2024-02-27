6918a5f42f8b6cc1addfdf68665a9236ae0d24a0
hyunjun <parkhyunjun00@gmail.com>
2024-02-27 11:04:11 +0900

    Performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import {
-  DragDropContext,
-  Draggable,
-  DropResult,
-  Droppable,
-} from "react-beautiful-dnd";
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
+import DraggableCard from "./Components/DraggableCard";
 
 const Wrapper = styled.div`
   display: flex;
@@ -32,29 +28,16 @@ const Board = styled.div`
   min-height: 200px;
 `;
 
-const Card = styled.div`
-  border-radius: 5px;
-  margin-bottom: 5px;
-  padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
-`;
-
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
     if (!destination) return;
     setToDos((oldToDos) => {
       const toDosCopy = [...oldToDos];
-      // 1)Delete item on sourceindex
-      console.log("Delete item on", source.index);
-      console.log(toDosCopy);
+      // 1)Delete item on source.index
       toDosCopy.splice(source.index, 1);
-      console.log("Deleted item");
-      console.log(toDosCopy);
       // 2)Put back the item on the destination.index
-      console.log("Put back", draggableId, "on ", destination.index);
       toDosCopy.splice(destination?.index, 0, draggableId);
-      console.log(toDosCopy);
       return toDosCopy;
     });
   };
@@ -66,17 +49,7 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable key={toDo} draggableId={toDo} index={index}>
-                    {(magic) => (
-                      <Card
-                        ref={magic.innerRef}
-                        {...magic.dragHandleProps}
-                        {...magic.draggableProps}
-                      >
-                        {toDo}
-                      </Card>
-                    )}
-                  </Draggable>
+                  <DraggableCard key={toDo} index={index} toDo={toDo} />
                 ))}
                 {magic.placeholder}
               </Board>
